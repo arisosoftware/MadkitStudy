62519126c921eba0e5d4e8bc257ba246029cf5d6
arisosoftware <arisosoftware@gmail.com>
2017-12-27 16:29:39 -0500

    version 1, added step 1,2,3 demo and bee world demo

---
+++ MarketReader/src/step2/MyScheduler02.java
@@ -0,0 +1,52 @@
+package step2;
+
+import madkit.gui.ConsoleAgent;
+import madkit.kernel.AbstractAgent;
+import madkit.kernel.Madkit;
+import madkit.kernel.Scheduler;
+import madkit.simulation.activator.GenericBehaviorActivator;
+
+/**
+ * #jws simulation.ex02.MyScheduler02 jws#
+ * 
+ * Let us have more fun by adding another simulated agent class with a different
+ * result for the doIt method and see what we now need. We need nothing else and
+ * just have to launch this new type of agent: Activators do not care about the
+ * exact type of the activated agents. Only the organizational position matters.
+ */
+
+public class MyScheduler02 extends Scheduler {
+
+	private GenericBehaviorActivator<AbstractAgent> activator1;
+
+	@Override
+	protected void activate() {
+		// 1 : create the simulation group
+		createGroup(SimulationModel.MY_COMMUNITY, SimulationModel.SIMU_GROUP);
+
+		// 2 : launch some simulated agents
+		for (int i = 0; i < 20; i++) {
+			launchAgent(new SimulatedAgent());
+			launchAgent(new SimulatedAgent02());
+		}
+
+		// 3 : initialize the activator
+		activator1 = new GenericBehaviorActivator<AbstractAgent>(
+				SimulationModel.MY_COMMUNITY, SimulationModel.SIMU_GROUP,
+				SimulationModel.ROLE, "doIt");
+		addActivator(activator1);
+
+		setDelay(300);
+	}
+
+	/**
+	 * A simple way of launching this scheduler
+	 * 
+	 * @param
+	 */
+	public static void main(String[] args) {
+		new Madkit("--launchAgents", MyScheduler02.class.getName() + ",true;"
+				+ ConsoleAgent.class.getName());
+	}
+
+}
---
+++ MarketReader/src/step3/MyScheduler06.java
@@ -0,0 +1,57 @@
+package step3;
+
+import step2.MyScheduler;
+import madkit.gui.ConsoleAgent;
+import madkit.kernel.AbstractAgent;
+import madkit.kernel.Madkit;
+import madkit.kernel.Scheduler;
+import madkit.simulation.activator.GenericBehaviorActivator;
+
+/**
+ * 
+ * 
+ * #jws simulation.ex06.MySimulationModel jws#
+ * 
+ * Nothing really new here, except that we define an additional Activator which
+ * is used to schedule the display. Especially, this is about calling the
+ * "observe" method of agents having the role of viewer in the organization
+ */
+
+public class MyScheduler06 extends Scheduler {
+
+	protected GenericBehaviorActivator<AbstractAgent> agents;
+	protected GenericBehaviorActivator<AbstractAgent> viewers;
+
+	@Override
+	protected void activate() {
+
+		// 1 : request my role
+		requestRole(MySimulationModel.MY_COMMUNITY,
+				MySimulationModel.SIMU_GROUP, MySimulationModel.SCH_ROLE);
+
+		// 3 : initialize the activators
+		// by default, they are activated once each in the order they have been
+		// added
+		agents = new GenericBehaviorActivator<AbstractAgent>(
+				MySimulationModel.MY_COMMUNITY, MySimulationModel.SIMU_GROUP,
+				MySimulationModel.AGENT_ROLE, "doIt");
+		addActivator(agents);
+		viewers = new GenericBehaviorActivator<AbstractAgent>(
+				MySimulationModel.MY_COMMUNITY, MySimulationModel.SIMU_GROUP,
+				MySimulationModel.VIEWER_ROLE, "observe");
+		addActivator(viewers);
+
+		setDelay(20);
+
+		// 4 : let us start the simulation automatically
+		setSimulationState(SimulationState.RUNNING);
+	}
+
+	/**
+	 * A simple way of launching this scheduler
+	 */
+	public static void main(String[] args) {
+		new Madkit("--launchAgents", MyScheduler06.class.getName() + ",true;"
+				+ ConsoleAgent.class.getName());
+	}
+}
